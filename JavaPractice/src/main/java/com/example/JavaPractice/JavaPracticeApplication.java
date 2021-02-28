0765e0c10794e3863a5ae14a4764c453f05067dc
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-27 23:52:27 -0600

    worked on max subset of sums not adajecent algorithm

--- JavaPractice/src/main/java/com/example/JavaPractice/JavaPracticeApplication.java
+++ JavaPractice/src/main/java/com/example/JavaPractice/JavaPracticeApplication.java
@@ -3,11 +3,15 @@ package com.example.JavaPractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import AlgoExpert.MaxSubsetSumNoAdjacent;
+
 @SpringBootApplication
 public class JavaPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaPracticeApplication.class, args);
+		int[] array = new int[]{75,105,120,75,90,135};
+		System.out.println(MaxSubsetSumNoAdjacent.maxSubsetSumNoAdjacent(array));
 	}
 
 }
