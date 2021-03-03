fb859ea46c94f957fc640c199bb3eaa6f377baac
John Rojcewicz <rojcewiczj@gmail.com>
2021-03-03 14:25:43 -0600

    AlgoExpert practie java

---
+++ JavaPractice/src/main/java/AlgoExpert/NonConstructibleChange.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class NonConstructibleChange {
+
+  public int nonConstructibleChange(int[] coins) {
+    Arrays.sort(coins);
+		
+		int currentChangeCreated = 0;
+		for(int coin: coins){
+			if(coin > currentChangeCreated + 1){
+				return currentChangeCreated + 1;
+			}
+			currentChangeCreated += coin;
+		}
+    return currentChangeCreated + 1;
+  }
+}
