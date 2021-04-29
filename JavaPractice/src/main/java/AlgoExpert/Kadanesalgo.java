098c699e1cbe7537a97a9abc918d40e970bd6af5
John Rojcewicz <rojcewiczj@gmail.com>
2021-04-29 13:58:00 -0500

    AlgoExpert Java

---
+++ JavaPractice/src/main/java/AlgoExpert/Kadanesalgo.java
@@ -0,0 +1,14 @@
+package AlgoExpert;
+
+public class Kadanesalgo {
+    public static int kadanesAlgorithm(int[] array) {
+        int maxEndingHere = array[0];
+            int maxSoFar = array[0];
+            for(int i = 1; i < array.length; i++){
+                int num = array[i];
+                maxEndingHere = Math.max(num, maxEndingHere + num);
+                maxSoFar = Math.max(maxSoFar, maxEndingHere);
+            }
+        return maxSoFar;
+      }
+}
