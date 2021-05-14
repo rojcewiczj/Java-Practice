60de13192c6459ed6b2fc45f92965c348b50df9c
John Rojcewicz <rojcewiczj@gmail.com>
2021-05-14 09:36:13 -0500

    Algos in Java

---
+++ JavaPractice/src/main/java/AlgoExpert/AlgoExpertMaxSumsAdj.java
@@ -0,0 +1,17 @@
+package AlgoExpert;
+
+public class AlgoExpertMaxSumsAdj {
+    public static int maxSubsetSumNoAdjacent(int[] array) {
+		if(array.length == 0){
+    	return 0;
+		} else if (array.length == 1){
+			return array[0];
+		}
+		int[] maxSums = array.clone();
+		maxSums[1] = Math.max(array[0], array[1]);
+		for(int i = 2; i < array.length; i++){
+			maxSums[i] = Math.max(maxSums[i-1], maxSums[i-2] + array[i]);
+		}
+		return maxSums[array.length -1];
+  }
+}
