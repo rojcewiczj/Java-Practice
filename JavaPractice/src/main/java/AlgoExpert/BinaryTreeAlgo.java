634e81ef8471c36b1454f504aa4d28df4b8d95a0
John Rojcewicz <rojcewiczj@gmail.com>
2021-05-14 09:11:22 -0500

    inserting an array of ints to create min height binary tree

---
+++ JavaPractice/src/main/java/AlgoExpert/BinaryTreeAlgo.java
@@ -0,0 +1,43 @@
+package AlgoExpert;
+
+public class BinaryTreeAlgo {
+    public static BST minHeightBst(List<Integer> array) {
+        return constructMinHeightBst(array, 0, array.size() -1);
+        return null;
+      }
+      public static BST constructMinHeightBst(List<Integer> array, int startIdx, int endIdx){
+            if(endIdx < startIdx) return null;
+            int midIdx = (startIdx + endIdx) / 2;
+            BST bst = new BST(array.get(midIdx));
+            bst.left = constructMinHeightBst(array, startIdx, midIdx -1);
+            bst.right = constructMinHeightBst(array, midIdx + 1, endIdx);
+            return bst;
+        }
+      static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+    
+        public BST(int value) {
+          this.value = value;
+          left = null;
+          right = null;
+        }
+    
+        public void insert(int value) {
+          if (value < this.value) {
+            if (left == null) {
+              left = new BST(value);
+            } else {
+              left.insert(value);
+            }
+          } else {
+            if (right == null) {
+              right = new BST(value);
+            } else {
+              right.insert(value);
+            }
+          }
+        }
+      }
+}
