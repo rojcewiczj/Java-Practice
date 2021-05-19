13d807ca5b66c3d9197c9748b4fb7e35d9cb5a06
John Rojcewicz <rojcewiczj@gmail.com>
2021-05-19 11:46:46 -0500

    java practice

---
+++ JavaPractice/src/main/java/AlgoExpert/AlgoExpert5_19_8.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Program {
+
+  public int[][] mergeOverlappingIntervals(int[][] intervals) {
+    int[][] sortedIntervals = intervals.clone();
+		Arrays.sort(sortedIntervals, (a,b) -> Integer.compare(a[0], b[0]));
+		
+		List<int[]> mergedIntervals = new ArrayList<int[]>();
+		int[] currentInterval = sortedIntervals[0];
+		mergedintervals.add(currentInterval);
+		
+		for(int[] nextInterval : sortedIntervals)
+			int currentIntervalEnd = currentInterval[1];
+			int nextIntervalStart = nextInterval[0];
+			int nextIntervalEnd = nextInterval[1];
+		
+		  if(currentIntervalEnd >= nextIntervalStart){
+				currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+			} else{
+				currentInterval = nextInterval;
+				mergedIntervals.add(currentInterval);
+			}
+	}
+	return mergedIntervals.toArray(new int[mergedIntervals.size()][]) ;
+  }
+}
