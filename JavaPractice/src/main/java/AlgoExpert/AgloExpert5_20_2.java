695cac03401b4a6c4623513b5247152dfb150bfc
John Rojcewicz <rojcewiczj@gmail.com>
2021-05-20 13:40:11 -0500

    java practice

---
+++ JavaPractice/src/main/java/AlgoExpert/AgloExpert5_20_2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Program {
+  public static int[] largestRange(int[] array) {
+    int[] bestRange = new int[2];
+		int longestLength = 0;
+		Map<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
+		for(int num : array){
+			nums.put(num, true;
+		}
+		for(int num : array){
+			if(!nums.get(num)){
+				continue;
+			}
+			nums.put(num, false);
+			int currentLength = 1;
+			int left = num -1;
+			int right = num + 1;
+			while(nums.containsKey(left)){
+				numps.put(left, false)'
+					currentLength++;
+					left--;
+			}
+			while(nums.containsKey(right)){
+				nums.put(right, false);
+				currentLength++;
+				right++;
+			}
+			if(currentLength > longestLength){
+				longestLength = currentLength;
+				bestRange = new int[] {left + 1. right -1};
+			}
+		}
+    return bestRange;
+  }
+}
