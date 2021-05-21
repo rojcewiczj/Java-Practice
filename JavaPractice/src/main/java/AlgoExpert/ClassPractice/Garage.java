1c1d4c58637febdcce4302e6301c6c1efcad8e4b
John Rojcewicz <rojcewiczj@gmail.com>
2021-05-21 13:37:27 -0500

    java practice

--- JavaPractice/src/main/java/AlgoExpert/ClassPractice/Garage.java
+++ JavaPractice/src/main/java/AlgoExpert/ClassPractice/Garage.java
@@ -8,4 +8,40 @@ public class Garage {
     public Garage(List<Vehicle> _vehicles){
         vehicles = _vehicles;
     }
+
+    public Integer TotalBill(){
+        Integer bill = 0;
+        for(Vehicle v : vehicles){
+            if (v instanceof MotorCycle){
+                bill += 10;
+            }
+            else{
+                bill += 30;
+            }
+        }
+        return bill;
+    }
+
+    public void addVehicle(Vehicle v){
+        vehicles.add(v);
+    }
+    
+    public Integer specificBill(Vehicle v){
+        Integer bill = 0;
+        if(v instanceof MotorCycle){
+            bill += 10;
+        }
+        else if(v instanceof Car){
+            bill += 30;
+        }
+        for(Vehicle find : vehicles){
+            if(find.name == v.name){
+                bill += 50;
+                return bill;
+            }
+        }
+        return 0;
+    }
 }
+
+
